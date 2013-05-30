719e42098e4d173f23aece89b6d0ad42c71d04e5
Roman Massey <romanmassey@gmail.com>
2013-05-29 22:17:22 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+
+<html lang='en'>
+
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8"/>
+	<link rel="stylesheet" href="style.css" type="text/css" media="screen">
+	<title>Shout!</title>
+</head>
+
+
+
+<body>
+</body>
+
+
+</html>
